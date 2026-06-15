86a4792c647c801fd6a6b0efc340225ea9957b70
sahilbhatia90 <sahilbhatia2609@gmail.com>
2026-06-15 16:06:45 +0530

    First Commit

---
+++ src/main/java/com/Booking/MountCab/MountCabApplication.java
@@ -0,0 +1,13 @@
+package com.Booking.MountCab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountCabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Booking/MountCab/MountCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Booking.MountCab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountCabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
